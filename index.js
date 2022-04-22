709549977795784482cae0ef99eb6d82c34267bf
AlexJr003 <alexandre.cirino003@gmail.com>
2022-04-21 21:15:27 -0300

    2nd commit

--- index.js
+++ index.js
@@ -1,40 +1,76 @@
 const express = require("express");
 const path = require("path");
+const { send } = require("process");
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
 
 const pokedex = [
-    {
-        id:1,
-        name: "Bulbasaur",
-        description:"There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
-        type: "Grass",
-        imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
-    },
-    {
-        id:2,
-        name: "Charmander",
-        description:"It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
-        type: "Fire",
-        imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png"
-    },
-    {
-        id:3,
-        name: "Squirtle ",
-        description:"When it retracts its long neck into its shell, it squirts out water with vigorous force.",
-        type: "Water",
-        imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png"
-    },
+  {
+    id: 1,
+    name: "Bulbasaur",
+    description:
+      "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
+    type: "Grass",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+  },
+  {
+    id: 2,
+    name: "Charmander",
+    description:
+      "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
+    type: "Fire",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+  },
+  {
+    id: 3,
+    name: "Squirtle ",
+    description:
+      "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
+    type: "Water",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+  },
+  {
+    id: 4,
+    name: "Pikachu",
+    description:
+      "Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy.",
+    type: "Electric",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+  },
 ];
 
-app.get("/", function (req, res) {
-  res.render("index", {pokedex});
+let pokemon = undefined;
+
+app.get("/", (req, res) => {
+  
+  res.render("index", { pokedex, pokemon });
 });
 
-app.get("/home", (req, res)=> {
-    res.send("Olá mundo do Express!");
-  });
+app.post("/add", (req, res) => {
+  const pokemon = req.body;
+  pokemon.id = pokedex.length + 1;
+  pokedex.push(pokemon);
+  res.redirect("/");
+});
 
-app.listen(3000, () => console.log("Servidor rodando em http://localhost:3000"));
+app.get("/details/:id", (req, res) => {
+    const id = +req.params.id;
+    pokemon = pokedex.find((pokemon) => pokemon.id === id);
+    res.redirect("/");
+});
+
+app.post("/update/:id", (req, res) => {
+    const id = +req.params.id - 1;
+    const newPokemon = req.body;
+    newPokemon.id = id + 1;
+    pokedex[id] = newPokemon;
+    pokemon = undefined;
+    res.redirect("/");
+});
+
+app.listen(3000, () =>
+  console.log("Servidor rodando em http://localhost:3000")
+);
